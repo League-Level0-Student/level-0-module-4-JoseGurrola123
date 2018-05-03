e90a5dfd5e5d38e4df04c490f02ef141398f0450
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-03 13:24:40 -0700

    not finished

---
+++ src/OwnAdventure.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class OwnAdventure {
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null,"your walking home and you come across a forest.");
+String[] options = {"Yes", "no"};
+int choice=	JOptionPane.showOptionDialog(null, "Do you want to explore the forest?", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+	if(choice== 1) { 
+		JOptionPane.showConfirmDialog(null, "You have reached your destination, home");
+}
+	else { JOptionPane.showConfirmDialog(null, "Great now lets start your adventure");
+	String[] options2 = { "get a water gun", "give him your money"};
+	int choice2=JOptionPane.showOptionDialog(null, "you started your adventure but a goblin jumps in front of you saying to give him all your money ", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options2, options2[0]);
+	
+	if(choice2== 0){ {String[] options3 = ("get a water gun", "give him your money"};
+	int choice3=JOptionPane.showOptionDialog(null, "you started your adventure but a goblin jumps in front of you saying to give him all your money ", "title", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options3, options3[0]);
+}
+	}
+	}
+}
+
+
+
