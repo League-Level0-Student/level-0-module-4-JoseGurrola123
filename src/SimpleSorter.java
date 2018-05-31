4941a9fbe21ecc71158756680df144e24ab6bc2b
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-31 13:25:32 -0700

    not finished

---
+++ src/SimpleSorter.java
@@ -0,0 +1,18 @@
+import java.awt.print.Printable;
+
+import javax.swing.JOptionPane;
+
+public class SimpleSorter {
+public static void main(String[] args) {
+	String num=JOptionPane.showInputDialog("Choose a number");
+	String num2=JOptionPane.showInputDialog("Choose another number");
+	String num3=JOptionPane.showInputDialog("Choose another number");
+	int ans=Integer.parseInt(num);
+	int ans2=Integer.parseInt(num2);
+	int ans3=Integer.parseInt(num3);
+	
+	if (ans<ans2&&ans<ans3) {
+		System.out.println(ans);
+	}
+}
+}
